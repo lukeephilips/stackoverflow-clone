c9acae7ff92d0eef2cbc0ef8ee8ca908971b680a
Luke Philips and Margie Nevarez <luke+margie@>
2017-02-14 09:58:19 -0800

    add user authentication

---
+++ stack_overflow/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      flash[:notice] =  "yay"
+      redirect_to '/'
+    else
+      flash[:notice] =  "boo"
+      redirect_to log_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "bye"
+    redirect_to '/'
+  end
+end
---
+++ stack_overflow/spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    password_hash "MyString"
+    password_salt "MyString"
+  end
+end
